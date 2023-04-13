ce0b14b1d5ab7310bdc7c003fe2a4b8ee5a5af32
Zaklina Radivojevic <zaklinaradivojevic77777777@gmail.com>
2023-04-13 21:54:56 +0100

    sesnaesti commit

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
 from flask import Flask, render_template, request, flash, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, logout_user
 
-app=Flask(__name__)
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
+app.config["SECRET_KEY"] = "abc"
+db = SQLAlchemy()
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+class Users(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(250), unique=True, nullable=False)
+    password = db.Column(db.String(250), nullable=False)
+ 
+ 
+db.init_app(app)
+ 
+ 
+with app.app_context():
+    db.create_all()
+ 
+ 
+@login_manager.user_loader
+def loader_user(user_id):
+    return Users.query.get(user_id)
+ 
+ 
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -26,6 +52,43 @@ def new_post():
     else:
         return render_template('new_post.html')
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    # If a post request was made, find the user by
+    # filtering for the username
+    if request.method == "POST":
+        user = Users.query.filter_by(
+            username=request.form.get("username")).first()
+        # Check if the password entered is the
+        # same as the user's password
+        if user.password == request.form.get("password"):
+            # Use the login_user method to log in the user
+            login_user(user)
+            return redirect(url_for("home"))
+        # Redirect the user back to the home
+        # (we'll create the home route in a moment)
+    return render_template("login.html")
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+  # If the user made a POST request, create a new user
+    if request.method == "POST":
+        user = Users(username=request.form.get("username"),
+                     password=request.form.get("password"))
+        # Add the user to the database
+        db.session.add(user)
+        # Commit the changes made
+        db.session.commit()
+        # Once user account created, redirect them
+        # to login route (created later on)
+        return redirect(url_for("login"))
+    # Renders sign_up template if user made a GET request
+    return render_template("sign_in.html")
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("home"))
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=1320)
