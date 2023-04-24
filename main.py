c5b8ff4d89d2cd230feb59a4c7d222ed1126f18d
Zaklina Radivojevic <zaklinaradivojevic77777777@gmail.com>
2023-04-24 14:53:41 +0100

    tridesetprvi commit

--- main.py
+++ main.py
@@ -6,30 +6,67 @@ from flask_migrate import Migrate
 
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 app.config["SECRET_KEY"] = "abc"
 app.config.from_object(DevConfig)
 
 db = SQLAlchemy(app)
-
+migrate = Migrate(app, db)
 class User(db.Model):
      __tablename__ = 'user_table_name'
 
      id = db.Column(db.Integer, primary_key=True)
      email = db.Column(db.String(255), unique=True, nullable=False)
-     username = db.Column(db.String(255), unique=True, nullable=False)
+     username = db.Column(db.String(255), nullable=False, index=True,unique=True)
      password = db.Column(db.String(255), nullable=False)
-
+     posts = db.relationship('Post', backref='user',lazy='dynamic')
+      
 def __init__(self, username):
  self.username = username
  
 def __repr__(self):
      return "<User '{}'>".format(self.username)
 
-with app.app_context():
-    
-    db.create_all()
+
+tags = db.Table('post_tags',
+ db.Column('post_id', db.Integer, db.ForeignKey('post.id')),
+ db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'))
+)
+class Post(db.Model):
+ id = db.Column(db.Integer(), primary_key=True)
+ title = db.Column(db.String(255), nullable=False)
+ text = db.Column(db.Text())
+ publish_date = db.Column(db.DateTime(), default=datetime.datetime.now)
+ comments = db.relationship('Comment',backref='post',lazy='dynamic')
+ user_id = db.Column(db.Integer(), db.ForeignKey('user.id'))
+ tags = db.relationship('Tag',secondary=tags,backref=db.backref('posts', lazy='dynamic'))
  
+def __init__(self, title):
+ self.title = title
+ 
+def __repr__(self):
+ return "<Post '{}'>".format(self.title)
+
+class Tag(db.Model):
+ id = db.Column(db.Integer(), primary_key=True)
+ title = db.Column(db.String(255), nullable=True, unique=True)
+ 
+def __init__(self, title):
+ self.title = title
+ 
+def __repr__(self):
+ return "<Tag '{}'>".format(self.title)
+
+class Comment(db.Model):
+ id = db.Column(db.Integer(), primary_key=True)
+ name = db.Column(db.String(255), nullable=False)
+ text = db.Column(db.Text())
+ date = db.Column(db.DateTime(), default=datetime.datetime.now)
+ post_id = db.Column(db.Integer(), db.ForeignKey('post.id'))
+ 
+def __repr__(self):
+ return "<Comment '{}'>".format(self.text[:15])
+
 
 @app.route('/')
 def home():    
@@ -57,8 +94,6 @@ def login():
     return render_template('login.html')
 
    
-
-
 @app.route('/register', methods=['GET', 'POST'])
 def register():
      return render_template('sign_in.html')
--- manage.py
+++ manage.py
@@ -1,8 +1,9 @@
-from main import app, db, User
+from main import app, db, User, Post, Tag, migrate
 
 @app.shell_context_processor
 def make_shell_context():
- return dict(app=app, db=db, User=User)
+ return dict(app=app, db=db, User=User, Post=Post, Tag=Tag,
+migrate=migrate)
 
 #$ export FLASK_APP=manage.py
 # $ flask shell
