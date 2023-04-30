55925b0b8421fef9c89ecf25ffb601e592911e81
Zaklina Radivojevic <zaklinaradivojevic77777777@gmail.com>
2023-04-30 20:16:45 +0100

    cetrdesetprvi commit

--- main.py
+++ main.py
@@ -8,71 +8,84 @@ from wtforms.validators import DataRequired, Length
 from sqlalchemy import desc, func
 from config import DevConfig
 
-
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
-app.config["SECRET_KEY"] = "abc"
 app.config.from_object(DevConfig)
-
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 
-class User(db.Model):
-     __tablename__ = 'user_table_name'
-
-     id = db.Column(db.Integer, primary_key=True)
-     email = db.Column(db.String(255), unique=True, nullable=False)
-     username = db.Column(db.String(255), nullable=False, index=True,unique=True)
-     password = db.Column(db.String(255), nullable=False)
-     posts = db.relationship('Post', backref='user',lazy='dynamic')
-     
-def __init__(self, username):
- self.username = username
- 
-def __repr__(self):
-     return "<User '{}'>".format(self.username)
-
-
-tags = db.Table('post_tags',
- db.Column('post_id', db.Integer, db.ForeignKey('post.id')),
- db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'))
+tags = db.Table(
+    'post_tags',
+    db.Column('post_id', db.Integer, db.ForeignKey('post.id')),
+    db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'))
 )
 
-class Post(db.Model):
- id = db.Column(db.Integer(), primary_key=True)
- title = db.Column(db.String(255), nullable=False)
- text = db.Column(db.Text())
- publish_date = db.Column(db.DateTime(), default=datetime.datetime.now)
- comments = db.relationship('Comment',backref='post',lazy='dynamic')
- user_id = db.Column(db.Integer(), db.ForeignKey('user.id'))
- tags = db.relationship('Tag',secondary=tags,backref=db.backref('posts', lazy='dynamic'))
- 
-def __init__(self, title):
- self.title = title
- 
-def __repr__(self):
- return "<Post '{}'>".format(self.title)
+blog_blueprint = Blueprint(
+    'blog',
+    __name__,
+    template_folder='templates/blog',
+    url_prefix="/blog"
+)
 
-class Tag(db.Model):
- id = db.Column(db.Integer(), primary_key=True)
- title = db.Column(db.String(255), nullable=True, unique=True)
- 
-def __init__(self, title):
- self.title = title
- 
-def __repr__(self):
- return "<Tag '{}'>".format(self.title)
+
+class User(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(255))
+    password = db.Column(db.String(255))
+    posts = db.relationship('Post', backref='user', lazy='dynamic')
+
+    def __init__(self, username=""):
+        self.username = username
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(255))
+    text = db.Column(db.Text())
+    publish_date = db.Column(db.DateTime())
+    user_id = db.Column(db.Integer(), db.ForeignKey('user.id'))
+    comments = db.relationship(
+        'Comment',
+        backref='post',
+        lazy='dynamic'
+    )
+    tags = db.relationship(
+        'Tag',
+        secondary=tags,
+        backref=db.backref('posts', lazy='dynamic')
+    )
+
+    def __init__(self, title=""):
+        self.title = title
+
+    def __repr__(self):
+        return "<Post '{}'>".format(self.title)
+
 
 class Comment(db.Model):
- id = db.Column(db.Integer(), primary_key=True)
- name = db.Column(db.String(255), nullable=False)
- text = db.Column(db.Text())
- date = db.Column(db.DateTime(), default=datetime.datetime.now)
- post_id = db.Column(db.Integer(), db.ForeignKey('post.id'))
- 
-def __repr__(self):
- return "<Comment '{}'>".format(self.text[:15])
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(255))
+    text = db.Column(db.Text())
+    date = db.Column(db.DateTime(), default=datetime.datetime.now)
+    post_id = db.Column(db.Integer(), db.ForeignKey('post.id'))
+
+    def __repr__(self):
+        return "<Comment '{}'>".format(self.text[:15])
+
+
+class Tag(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(255))
+
+    def __init__(self, title=""):
+        self.title = title
+
+    def __repr__(self):
+        return "<Tag '{}'>".format(self.title)
+
 
 class CommentForm(Form):
     name = StringField(
@@ -90,9 +103,16 @@ def sidebar_data():
 
     return recent, top_tags
 
+
 @app.route('/')
-def home():
-    posts = Post.query.order_by(Post.publish_date.desc()).paginate()
+def index():
+    return redirect(url_for('home'))
+
+
+@blog_blueprint.route('/')
+@blog_blueprint.route('/<int:page>')
+def home(page=1):
+    posts = Post.query.order_by(Post.publish_date.desc()).paginate(page, app.config.get('POSTS_PER_PAGE', 10), False)
     recent, top_tags = sidebar_data()
 
     return render_template(
@@ -102,8 +122,8 @@ def home():
         top_tags=top_tags
     )
 
-    
-@app.route('/post/<int:post_id>', methods=('GET', 'POST'))
+
+@blog_blueprint.route('/post/<int:post_id>', methods=('GET', 'POST'))
 def post(post_id):
     form = CommentForm()
 
@@ -120,13 +140,13 @@ def post(post_id):
             db.session.rollback()
         else:
             flash('Comment added', 'info')
-        return redirect(url_for('post', post_id=post_id))
+        return redirect(url_for('blog.post', post_id=post_id))
 
     post = Post.query.get_or_404(post_id)
     tags = post.tags
     comments = post.comments.order_by(Comment.date.desc()).all()
     recent, top_tags = sidebar_data()
-    
+
     return render_template(
         'post.html',
         post=post,
@@ -137,7 +157,8 @@ def post(post_id):
         form=form
     )
 
-@app.route('/posts_by_tag/<string:tag_name>')
+
+@blog_blueprint.route('/posts_by_tag/<string:tag_name>')
 def posts_by_tag(tag_name):
     tag = Tag.query.filter_by(title=tag_name).first_or_404()
     posts = tag.posts.order_by(Post.publish_date.desc()).all()
@@ -151,7 +172,8 @@ def posts_by_tag(tag_name):
         top_tags=top_tags
     )
 
-@app.route('/posts_by_user/<string:username>')
+
+@blog_blueprint.route('/posts_by_user/<string:username>')
 def posts_by_user(username):
     user = User.query.filter_by(username=username).first_or_404()
     posts = user.posts.order_by(Post.publish_date.desc()).all()
@@ -166,6 +188,7 @@ def posts_by_user(username):
     )
 
 from flask.views import View
+
 
 class GenericListView(View):
 
@@ -187,6 +210,7 @@ class GenericListView(View):
                    'columns': self.columns}
         return self.render_template(context)
 
+
 app.add_url_rule(
     '/generic_posts', view_func=GenericListView.as_view(
         'generic_posts', model=Post)
@@ -203,6 +227,7 @@ app.add_url_rule(
 )
 
 
+app.register_blueprint(blog_blueprint)
 
 
 @app.errorhandler(404)
